d19966d45dad3e50314a69a669e68f0dee44ebac
Jessica Robertson and Micky Matthews <jessica@>
2017-02-27 20:49:52 -0800

    Correct JS functions to concat each array into one.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-var favThings = favFoodsInput.concat(favColorsInput, favAnimalsInput)
-
+var concatFavs = function (fav1, fav2, fav3) {
+   return fav1 + ", " + fav2 + ", " + fav3;
+};
 
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
+  $("form#questions").submit(function(event) {
+    event.preventDefault();
     var favFoodsInput = $("input#favFood").val();
     var favColorsInput = $("input#favColor").val();
     var favAnimalsInput= $("input#favAnimal").val();
+    var favThings = concatFavs(favFoodsInput, favColorsInput, favAnimalsInput);
+    $("#favThingsOutput").text(favThings);
+    console.log(favThings)
 
-
-
-    $("#output").show(favThings);
-
-    event.preventDefault();
   });
 });
